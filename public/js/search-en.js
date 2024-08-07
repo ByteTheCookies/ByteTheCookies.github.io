bae100f91c70e4577ba6f76a5a5786f02536ddda
Akiidjk <dragonfaiver@gmail.com>
2024-08-07 16:34:15 +0200

    Update URLs to use GitHub Pages instead of localhost

--- public/js/search-en.js
+++ public/js/search-en.js
@@ -1,7 +1,7 @@
 (() => {
   // <stdin>
   (() => {
-    var index_url = "http://localhost:1313/index.json";
+    var index_url = "https://bytethecookies.github.io/index.json";
     var cmp = new Intl.Collator("en", { numeric: true, sensitivity: "base" }).compare;
     var inf = Infinity;
     var escapeRegExp = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
