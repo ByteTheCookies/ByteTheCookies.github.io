52f3fec47261e3962e47b816153da25fb627df0a
akiidjk <dragonfaiver@gmail.com>
2026-01-18 12:22:42 +0100

    Move favicon files to public/favicon

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,7 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		{
-			src: "/favicon/favicon.ico", // Path of the favicon, relative to the /public directory
+			src: "favicon/favicon.ico", // Path of the favicon, relative to the /public directory
 			// theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 			// sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		},
