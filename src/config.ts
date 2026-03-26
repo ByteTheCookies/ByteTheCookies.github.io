120d235c4709e576c8ef52baf356d394eee747a3
akiidjk <akiidjk@proton.me>
2026-03-26 21:30:48 +0100

    Enable site banner, replace image and add credit

--- src/config.ts
+++ src/config.ts
@@ -16,12 +16,12 @@ export const siteConfig: SiteConfig = {
 		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "assets/images/banner.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
-		credit: {
+    credit: {
 			enable: true, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
+			text: "Alessandro Manfredi", // Credit text to be displayed
 			url: "", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
